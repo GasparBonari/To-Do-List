fda2554247f111022d045d8dbbee05bc8af9abbb
Gaspar Bonari <89667053+GasparBonari@users.noreply.github.com>
2023-06-27 15:28:26 +0100

    .

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
 "use strict";
+
+// DATA
+
+let user1 = 
+{
+    name: "John",
+    lastname: "Wick",
+    pin: 1111,
+    list: ["Check garden", "Pick up post"],
+}
+
+let user2 = 
+{
+    name: "Maria",
+    lastname: "Lash",
+    pin: 2222,
+    list: [],
+}
+
+let user3 =
+{
+    name: "Jack",
+    lastname: "Sparrow",
+    pin: 3333,
+    list: ["Meeting with teacher"],
+}
+
+// SELECTORS
+
+let users = [user1, user2, user3];
+let currentAcount;
+
+const usernameInput = document.querySelector(".username-input");
+const passwordInput = document.querySelector(".password-input");
+const btnLogIn = document.querySelector(".log-in-button");
+
+
+// CREATE USERNAMES FROM CURRENT USERS
+
+function createUserNames(clients)
+{
+    for(let i of clients)
+    {
+        let fullName = [i.name.toLowerCase(), i.lastname.toLowerCase()]
+    
+        i.username = fullName.map(e => e[0]).join("")
+    }
+}
+
+createUserNames(users);
+
+
+// LOG IN
+
+btnLogIn.addEventListener("click", function(e)
+{
+    e.preventDefault();
+
+    currentAcount = users.find(e => e.username == usernameInput.value);
+
+    if(currentAcount.username == usernameInput.value && currentAcount.pin == passwordInput.value)
+    {
+        console.log("in")
+    }
+
+})
